21ae7cc0793ab75d2dcf2d3299db50f61aec1325
Felipe Najson <felipe@team.northtwofive.com>
2021-09-13 09:03:39 -0300

    ConsentManager reverted to segment version

--- src/components/consentBanner.jsx
+++ src/components/consentBanner.jsx
@@ -1,40 +1,43 @@
-import React from "react";
-import { ConsentManager, openConsentManager } from "@segment/consent-manager";
-import inEU from "@segment/in-eu";
+import React from 'react';
+import { ConsentManager, openConsentManager } from '@segment/consent-manager';
+import inEU from '@segment/in-eu';
 
 export default function ConsentBanner() {
   const bannerContent = (
     <span>
       We use cookies (and other similar technologies) to collect data to improve
       your experience on our site. By using our website, you’re agreeing to the
-      collection of data as described in our{" "}
-      <a href="/docs/legal/website-data-collection-policy/" target="_blank">
+      collection of data as described in our{' '}
+      <a href='/docs/legal/website-data-collection-policy/' target='_blank'>
         Website Data Collection Policy
       </a>
       .
     </span>
   );
-  const bannerSubContent = "You can change your preferences at any time.";
-  const preferencesDialogTitle = "Website Data Collection Preferences";
+  const bannerSubContent = 'You can change your preferences at any time.';
+  const preferencesDialogTitle = 'Website Data Collection Preferences';
   const preferencesDialogContent =
-    "We use data collected by cookies and JavaScript libraries to improve your browsing experience, analyze site traffic, deliver personalized advertisements, and increase the overall performance of our site.";
-  const cancelDialogTitle = "Are you sure you want to cancel?";
+    'We use data collected by cookies and JavaScript libraries to improve your browsing experience, analyze site traffic, deliver personalized advertisements, and increase the overall performance of our site.';
+  const cancelDialogTitle = 'Are you sure you want to cancel?';
   const cancelDialogContent =
-    "Your preferences have not been saved. By continuing to use our website, you՚re agreeing to our Website Data Collection Policy.";
+    'Your preferences have not been saved. By continuing to use our website, you՚re agreeing to our Website Data Collection Policy.';
 
   return (
     <div>
       <ConsentManager
-        writeKey="5V8KznnIFIDh1ejQLbmX7ikfSRa6r8bF"
+        writeKey='5V8KznnIFIDh1ejQLbmX7ikfSRa6r8bF'
         bannerContent={bannerContent}
         bannerSubContent={bannerSubContent}
         preferencesDialogTitle={preferencesDialogTitle}
         preferencesDialogContent={preferencesDialogContent}
         cancelDialogTitle={cancelDialogTitle}
         cancelDialogContent={cancelDialogContent}
+        bannerActionsBlock={true}
+        bannerHideCloseButton={true}
+        bannerAsModal={true}
       />
 
-      <button type="button" onClick={openConsentManager}>
+      <button type='button' onClick={openConsentManager}>
         Website Data Collection Preferences
       </button>
     </div>
